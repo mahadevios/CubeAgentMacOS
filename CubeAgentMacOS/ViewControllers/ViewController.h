a8d20c6d91ed878658ab89a98a9ae574e89bc54c
mac <admin@mahadev.local>
2018-11-23 18:16:51 +0530

    update

--- CubeAgentMacOS/ViewControllers/ViewController.h
+++ CubeAgentMacOS/ViewControllers/ViewController.h
@@ -1,12 +1,13 @@
 //
 //  ViewController.h
-//  CubeAgentMacOS
+//  CubeAgent
 //
-//  Created by mac on 23/11/18.
+//  Created by mac on 20/11/18.
 //  Copyright © 2018 Xanadutec. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
+#import "APIManager.h"
 
 @interface ViewController : NSViewController
 
---
+++ CubeAgentMacOS/ViewControllers/ViewController.m
@@ -0,0 +1,43 @@
+//
+//  ViewController.m
+//  CubeAgent
+//
+//  Created by mac on 20/11/18.
+//  Copyright © 2018 Xanadutec. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "Keychain.h"
+
+
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    NSString*  macId=[Keychain getStringForKey:@"udid"];
+    
+    
+    //    NSLog(@"doKeyForPassword: %@",key);
+    //    [[APIManager sharedManager] checkAPI];
+    
+    //    [[APIManager sharedManager] authenticateUserMacIDLocal:@"DDCF3B2D-362B-4C81-8AB3-DD56D49E5365" password:@"d" username:@"SAN"];
+    
+    [[APIManager sharedManager] updateDeviceMacID:@"DDCF3B2D-362B-4C81-8AB3-DD56D49E5365" password:@"d" username:@"SAN"];
+    
+    
+    // Do any additional setup after loading the view.
+}
+
+
+- (void)setRepresentedObject:(id)representedObject
+{
+    [super setRepresentedObject:representedObject];
+    
+    // Update the view, if already loaded.
+}
+
+
+@end
